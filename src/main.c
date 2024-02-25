8c56f11c1cf20ae9ffe1d3e771a72f8b9fad0e5b
ethanbowering24 <ethanbowering24@gmail.com>
2024-02-24 22:28:26 -0500

    setup wifi

--- src/main.c
+++ src/main.c
@@ -1,1 +1,12 @@
-void app_main() {}
+#include "esp_system.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+static const char *TAG = "main";
+
+void app_main() {
+
+    wifi_init();
+    
+}
